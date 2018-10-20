e7bfdc533b15a9273ad6100bcada497621c310e6
Andrew Webb <andrew@awebb.info>
2018-10-20 17:11:37 +0100

    30 fps

--- cam.py
+++ cam.py
@@ -2,15 +2,15 @@ from cv2 import VideoCapture, resize
 import asciify
 
 cam = VideoCapture(0)
-cam.set(3, 120)
-cam.set(4, 160)
+cam.set(3, 180)
+cam.set(4, 240)
 
 while True:
 
     s, img = cam.read()
 
     if s:
-        img = resize(img, (160, 58))
+        img = resize(img, (240, 87))
         asciify.asciify(img)
 
 cam.release()
