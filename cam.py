7a37e3c4d952d03965478b4053ab66ba89fe4c2f
Gergely Bencsik <becsegergely@gmail.com>
2021-10-23 19:40:33 +0200

    Examples added.

--- cam.py
+++ cam.py
@@ -2,15 +2,15 @@ from cv2 import VideoCapture, resize, flip
 import asciify
 
 cam = VideoCapture(0)
-cam.set(3, 180)
-cam.set(4, 240)
+cam.set(3, 600)
+cam.set(4, 800)
 
 while True:
 
-    s, img = cam.read()
+	s, img = cam.read()
 
-    if s:
-        img = flip(resize(img, (40, 20)), 1)
-        asciify.asciify(img)
+	if s:
+		img = flip(resize(img, (80, 35)), 1)
+		asciify.asciify(img)
 
 cam.release()
