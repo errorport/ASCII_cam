e5e5d938b725a43019014c727c2cb423438edbaa
Gergely Bencsik <becsegergely@gmail.com>
2021-11-21 14:31:57 +0100

    NCurses removed.

--- asciifyer.cpp
+++ asciifyer.cpp
@@ -1,9 +1,16 @@
 #include <opencv2/core.hpp>
-#include <ncurses.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
 
-int main() {
+int main(void) {
+	struct winsize w;
+	while(1) {
+		ioctl(0, TIOCGWINSZ, &w);
+		printf ("lines %d\n", w.ws_row);
+		printf ("columns %d\n", w.ws_col);
+	}
 	return 0;
 }
 
