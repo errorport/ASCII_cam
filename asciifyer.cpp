478c1198b15200a58e40c233b3977a0a2fdd268b
Gergely Bencsik <becsegergely@gmail.com>
2021-11-21 21:47:01 +0100

    Multithreaded prepared.

--- asciifyer.cpp
+++ asciifyer.cpp
@@ -7,6 +7,9 @@
 #include <stdio.h>
 #include <chrono>
 #include <sys/ioctl.h>
+#include <wchar.h>
+#include <string.h>
+#include <locale.h>
 
 #include <config.hpp>
 
@@ -14,7 +17,15 @@ using namespace std;
 
 void get_winsize(winsize* w);
 
+uint8_t colorize(uint8_t hue_value);
+
+wchar_t asciify(uint8_t value);
+
+void insert_character(wchar_t*, uint16_t*, cv::Vec3b);
+
 int main(void) {
+	setlocale(LC_ALL, "en_US.UTF-8");
+	fwide(stdout, 1);
 	cv::VideoCapture cap;
 	cap.open(0);
 	if(cap.isOpened()){
@@ -23,12 +34,23 @@ int main(void) {
 		struct winsize w;
 		chrono::_V2::system_clock::time_point t_start;
 		chrono::_V2::system_clock::time_point t_end;
+		uint32_t frame_size = 0;
 		while(1) {
 			t_start = chrono::high_resolution_clock::now();
 			cap.read(input_frame);
 			if (!input_frame.empty()){
 				get_winsize(&w);
-				cv::resize(input_frame, resized_frame, cv::Size(w.ws_row, w.ws_col));
+				cv::resize(input_frame, resized_frame, cv::Size(w.ws_col, w.ws_row));
+				cv:: normalize(resized_frame, resized_frame, 255, 0, cv::NORM_MINMAX, -1, cv::noArray());
+				cv::cvtColor(resized_frame, resized_frame, cv::COLOR_BGR2HSV);
+				for(size_t hidx = 0; hidx < resized_frame.rows; hidx++) {
+					wchar_t line_str[LINE_LENGTH];
+					uint16_t ch_idx = 0;
+					for(size_t widx = 0; widx < resized_frame.cols; widx++) {
+						insert_character(line_str, &ch_idx, resized_frame.at<cv::Vec3b>(hidx, widx)[1]);
+					}
+					wprintf(L"\n\r%ls", line_str);
+				}
 			}
 			t_end = chrono::high_resolution_clock::now();
 			//printf("D: %dms\n\r", chrono::duration_cast<chrono::microseconds>(t_end - t_start));
@@ -43,4 +65,27 @@ void get_winsize(winsize* w) {
 	ioctl(0, TIOCGWINSZ, w);
 	//printf("lines %d\n", w->ws_row);
 	//printf("columns %d\n", w->ws_col);
+}
+
+uint8_t colorize(uint8_t hue_value) {
+	return color_prefixes[
+		int((hue_offset + (hue_value * COLOR_COUNT * hue_gain) / 360) % COLOR_COUNT)
+	];
+}
+
+wchar_t asciify(uint8_t value) {
+	for(size_t step_idx = 0; step_idx < STEP_COUNT; step_idx++) {
+		if(value <= brightness_steps[step_idx]) {
+			return characters[step_idx];
+		}
+	}
+	return characters[0];
+}
+
+void insert_character(wchar_t* line_str, uint16_t* ch_idx, cv::Vec3b pixel) {
+	*ch_idx += swprintf(
+		line_str + *ch_idx
+		, CHAR_LENGTH
+		, L"\x1b[%dm%lc\x1b[0m", colorize(pixel[1]), asciify(pixel[0])
+	);
 }
--- config.hpp
+++ config.hpp
@@ -2,15 +2,23 @@
 #define __CONFIG_HPP__
 
 #include <stdint.h>
+#include <wchar.h>
+
+// OPT
+#define COLOR_COUNT	6
+#define STEP_COUNT	9
+#define LINE_LENGTH 1024
+#define CHAR_LENGTH 16
 
 extern uint8_t hue_offset	= 1;
 extern uint8_t hue_gain		= 10;
 
-extern char characters[9] = {'█', '▓', '▒', '░', '•', '×', ':', '·', ' '};
+extern wchar_t characters[STEP_COUNT] = {L'█', L'▓', L'▒', L'░', L'•', L'×', L':', L'·', L' '};
 
-extern uint8_t brightness_steps[9] = {0, 20, 40, 60, 80, 100, 120, 140, 255};
+//extern uint8_t brightness_steps[STEP_COUNT] = {255, 140, 120, 100, 80, 60, 40, 20, 0};
+extern uint8_t brightness_steps[STEP_COUNT] = {0, 20, 40, 60, 100, 120, 140, 255};
 
-extern uint8_t color_prefixes[6] = {
+extern uint8_t color_prefixes[COLOR_COUNT] = {
 	31, // RED
 	33, // YELLOW
 	32, // GREEN
